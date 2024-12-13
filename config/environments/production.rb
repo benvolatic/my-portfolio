3b1f9f0c2fbbb77e0f862803e9dad45ba451a399
ben nelson <ben.volatic@gmail.com>
2024-12-12 22:20:40 -0800

    fixing .rb files

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Rails.application.configure do
 
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present? || true
 
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
