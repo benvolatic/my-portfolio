9d981be193a736a3e8e9635a772eac1cd0badb6b
ben nelson <ben.volatic@gmail.com>
2024-12-17 20:43:29 -0800

    fixed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Allow requests from your frontend in production
-    origins 'https://benjamin-react-rails-portfolio.herokuapp.com', 'http://localhost:3000'
+    origins 'https://benjamin-react-rails-portfolio.herokuapp.com', 'http://localhost:3000', 'https://volatic.org', 'https://www.volatic.org'
 
     resource '*',
              headers: :any,
              methods: %i[get post put patch delete options head],
-             credentials: false # Set credentials to false for wildcard or specify allowed origins
+             credentials: true # Set credentials to false for wildcard or specify allowed origins
   end
 end
